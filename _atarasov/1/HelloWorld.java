c884005aaa7c4fc8aec2f1ba097a0fd5c89cef32
atarasov <atarasov@mera.ru>
2017-11-03 13:33:33 +0300

    Comments

--- _atarasov/1/HelloWorld.java
+++ _atarasov/1/HelloWorld.java
@@ -1,5 +1,12 @@
+/**
+ * Это тестовый класс, который просто выводит Hello! на экран
+ */
 public class HelloWorld {
 
+    /**
+     * Функция main, с которой начинается исполнению любой программы
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
--- _atarasov/2/ArrayExample.java
+++ _atarasov/2/ArrayExample.java
@@ -1,3 +1,7 @@
+/**
+ * Пример работы с массивом, используя цикл for.
+ * Выводит числа от 1 до 10 с их номером в массиве.
+ */
 public class ArrayExample {
     public static void main(String[] args) {
         int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
