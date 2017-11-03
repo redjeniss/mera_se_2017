1815a2af77d855a35aadd3f76d2bad746feed82e
atarasov <atarasov@mera.ru>
2017-11-03 13:27:10 +0300

    Initial commit

---
+++ _atarasov/2/ArrayExample.java
@@ -0,0 +1,9 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Элемент с номером " + i + " равен " + array[i]);
+        }
+    }
+}
