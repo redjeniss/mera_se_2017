18048117bb158bb006f9e369dc25c6d06c4fb04c
Yana Mishina <janekov7@gmail.com>
2017-11-06 00:28:00 +0300

    Fix issues after review

--- mishina/2/Prog3.java
+++ mishina/2/Prog3.java
@@ -1,10 +1,10 @@
 public class Prog3 {
     public static void main(String[] args) {
         int massiv[] = {1, 5, 10, 2, 3, 100, 10, 11, 3, -5};
-        int maxValue = 0;
+        int maxValue = massiv[0];
         int maxIndex = 0;
-        for (int i = 0; i < massiv.length; i++) {
-            if (maxValue <= massiv[i]) {
+        for (int i = 1; i < massiv.length; i++) {
+            if (maxValue < massiv[i]) {
                 maxValue = massiv[i];
                 maxIndex = i;
             }
