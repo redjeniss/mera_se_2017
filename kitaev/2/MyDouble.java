47dca8fb6a2863a6fce02583def50a7b3270d7d4
Vladimir Kitaev <vlkitaev@tieto.mera.ru>
2017-11-08 09:08:01 +0300

    Hometask 2. Kitaev

--- kitaev/2/MyDouble.java
+++ kitaev/2/MyDouble.java
@@ -1,25 +1,41 @@
 package com.company;
 
-public class Exercise3 {
-    public static void main(String[] args) {
-        MyDouble var0 = new MyDouble(123123123d);
-        MyDouble var1 = new MyDouble(-1234123422d);
+public final class MyDouble {
+    private final double number;
 
-        if (var0.isEven()) {
-            MyDouble[] array = {var0, var1};
-            System.out.println("var0 is even, the sum is " + MyDouble.sum(array));
+    public MyDouble(double number) {
+        this.number = number;
+    }
+
+    public boolean isEven() {
+        return this.number % 2 == 0;
+    }
+
+    public boolean isPositive() {
+        return this.number > 0;
+    }
+
+    public static double sum(MyDouble[] args) {
+        double sum = 0;
+        for (MyDouble item : args) {
+            sum += item.number;
         }
-        if (var1.isEven()) {
-            System.out.println("var1 is even, the difference is " + MyDouble.difference(var0, var1));
-            // var1 is even, the difference is 1.357246545E9
+        return sum;
+    }
+
+    public static double difference(MyDouble a, MyDouble b) {
+        return a.number - b.number;
+    }
+
+    public static double multiply(MyDouble[] args) {
+        double composition = 1;
+        for (MyDouble item : args) {
+            composition *= item.number;
         }
-        if (var0.isPositive()) {
-            MyDouble[] array = {var0, var1};
-            System.out.println("var0 is positive, the composition is " + MyDouble.multiply(array));
-            // var0 is positive, the composition is -1.51949129884086912E17
-        }
-        if (var1.isPositive()) {
-            System.out.println("var1 is positive, the quotient is " + MyDouble.divide(var0, var1));
-        }
+        return composition;
+    }
+
+    public static double divide(MyDouble a, MyDouble b) {
+        return a.number / b.number;
     }
 }
