149c3f4dabc592f1c39cb94f454fed1385876472
Vashurova <redjeniss@gmail.com>
2017-11-09 23:49:25 +0300

    First and second lectures: Program, Task for Long

---
+++ vashurova/0/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        int i = 0;
+        for(i = (args.length - 1); i >= 0; i--){
+            System.out.print(args[i] + " ");
+        }
+    }
+}
